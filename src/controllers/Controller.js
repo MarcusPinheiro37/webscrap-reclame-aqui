191dc7d105fd4f4b2b856d82465d1fecc43ac839
Marcola <marcusmagno34@gmail.com>
2024-04-16 12:39:39 -0300

    Adicionado controlador de upsert (se existir algum url igual atualiza se nao cria um novo

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -11,7 +11,20 @@ export default class Controller{
             const dadosInseridos = await this.entidadeService.postRegistros(dados);
             return {message: 'Dados inseridos com sucesso', dadosInseridos };
         } else {
-            throw new Error();
+            throw new Error('Dados para post são inválidos ou campo chave não fornecidos');
+        }
+    }
+    async upsertDados(dados) {
+        if (Object.keys(dados).length !== 0) {
+            const { instance, created } = await this.entidadeService.upsertRegistro(dados);
+            
+            if (created) {
+                return { message: 'Novo registro criado com sucesso', instance };
+            } else {
+                return { message: 'Registro existente atualizado com sucesso', instance };
+            }
+        } else {
+            throw new Error('Dados para upsert são inválidos ou campos chave não fornecidos.');
         }
     }
 }
